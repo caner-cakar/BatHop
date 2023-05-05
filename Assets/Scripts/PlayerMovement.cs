8895e954651c968a8867f40267c897e11661d765
caner-cakar <cakar_caner@outlook.com>
2023-05-05 15:50:51 +0300

    Jump in the middle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField]private Collision2D collision2D;
+
+
     public float jumpY = 5f; 
     public float jumpX = -1f;
-
+    private bool isCenter;
     private bool isJumping;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Centering(collision2D);
     }
 
     void Update()
@@ -39,9 +43,28 @@ public class PlayerMovement : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D other) 
     {
-        if(other.gameObject.tag == "Ground")
+        if(other.gameObject.tag == "Ground" || other.gameObject.tag =="NormalPlatform" 
+        || other.gameObject.tag =="AppearingPlatform"|| other.gameObject.tag =="BrokenPlatform")
         {
             isJumping = false;
+            Centering(other);
         }
+    }
+
+    private void Centering(Collision2D other)
+    {
+        isCenter=false;
+            if(!isCenter)
+            {
+                Vector3 characterPos = transform.position; // karakterin pozisyonunu al
+                Vector3 centerPos = other.transform.position; // hedef nesnenin pozisyonunu al
+                characterPos.x = centerPos.x; // karakterin x pozisyonunu hedef nesnenin x pozisyonuna eşitle
+                transform.position = characterPos; // karakterin pozisyonunu güncelle
+                isCenter = true;
+            }
+            else
+            {
+                isCenter = false;
+            }
     }
 }
