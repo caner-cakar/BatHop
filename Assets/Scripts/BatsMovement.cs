e9831da505d6585f6cf95545a0abb01c0fe5bb62
caner-cakar <cakar_caner@outlook.com>
2023-07-03 11:43:37 +0300

    bats movement

--- Assets/Scripts/BatsMovement.cs
+++ Assets/Scripts/BatsMovement.cs
@@ -33,7 +33,7 @@ public class BatsMovement : MonoBehaviour
     {
         MoveTowardsTarget();
         StartCoroutine(ShakeBats());
-        if(scoreController.currentScore>visibleScore+10f && onPlayer)
+        if(scoreController.currentScore>visibleScore+20f && onPlayer)
         {
             BatsGetAway();
         }
